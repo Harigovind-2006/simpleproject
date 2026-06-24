e118e42bf3d34c247c8513ebd8ce00967afeb924
Harigovind M <harigovind.m@zeronetraining.local>
2026-06-24 13:20:41 +0530

    routing in frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/signup" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
