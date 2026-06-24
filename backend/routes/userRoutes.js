6d46bc183c8fd7e61b8cc0ea57997e02ca393e48
Harigovind M <harigovind.m@zeronetraining.local>
2026-06-24 13:31:53 +0530

    router updated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,22 +1,14 @@
 const express = require("express");
-const { userSignup,userLogin } = require("../controllers/userController");
-const {authMiddleware} = require("../middlewares/userMiddleWare")
+const { userSignup, userLogin } = require("../controllers/userController");
+const { authMiddleware } = require("../middlewares/userMiddleWare");
+
 const router = express.Router();
-
-// userRoutes.js
-router.get("/signup", (req, res) => {
-    res.render("signup");
-});
-router.get("/login", (req, res) => {
-    res.render("login");
-});
-router.get("/profile", authMiddleware, (req, res) => {
-    res.json(req.user);
-});
-
 
 router.post("/signup", userSignup);
 router.post("/login", userLogin);
 
+router.get("/profile", authMiddleware, (req, res) => {
+  res.json(req.user);
+});
 
 module.exports = router;
